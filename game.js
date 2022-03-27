f0f9014666155fabceb087b0c31bdfc1a304a8f0
Kelvin Chaves <kelvin.chaves@rockcontent.com>
2022-03-27 09:35:50 -0300

    Add animation

--- game.js
+++ game.js
@@ -1,16 +1,20 @@
 const gameContainer = document.getElementById("game")
 
+const generateRandomId = () => Math.random().toString().slice(2)
+
 let gameState = [
-    [2, 0, 0, 2],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [{value:0}, {value:0}, {value:0}, {value:0}],
+    [{value:0}, {value:0}, {value:0}, {value:0}],
+    [{value:0}, {value:0}, {value:0}, {value:0}],
+    [{value:0}, {value:0}, {value:0}, {value:0}],
 ]
 
+
 const padArrayWithZeros = (arr, atStart) => {
-    const zeros = new Array(4 - arr.length).fill(0)
+    const zeros = new Array(4 - arr.filter(({ value }) => value).length).fill({value: 0})
     return atStart ? zeros.concat(arr) : arr.concat(zeros)
 }
+
 
 const getColumn = (x, gameState) => ([
     gameState[0][x],
@@ -18,6 +22,10 @@ const getColumn = (x, gameState) => ([
     gameState[2][x],
     gameState[3][x],
 ])
+
+const getCellTransition = (x, y) => {
+    return `translateX(calc(600px * ${x * 0.25})) translateY(calc(600px * ${y * 0.25}))`
+}
 
 const handlers = {
     ArrowUp: () => {
@@ -43,18 +51,20 @@ const handlers = {
 const resolveRow = (row, reverse) => {
     const orderedRow = reverse ? [...row].reverse() : row;
 
-    let lastValue;
+    let lastCell;
 
-    const reducedRow = orderedRow.reduce((newRow, value, x) => {
-        if (lastValue && lastValue === value) {
-            newRow.push(value * 2)
-            lastValue = undefined
-        } else if (value && lastValue !== value && x !== 3) {
-            if (lastValue) newRow.push(lastValue)
-            lastValue = value;
+    const reducedRow = orderedRow.reduce((newRow, cell, x) => {
+        const lastValue = lastCell && lastCell.value;
+
+        if (lastValue && lastValue === cell.value) {
+            newRow.push({ ...cell, value: cell.value * 2 })
+            lastCell = undefined
+        } else if (cell.value && lastValue !== cell.value && x !== 3) {
+            if (lastValue) newRow.push(lastCell)
+            lastCell = cell;
         } else if (x === 3) {
-            if (lastValue) newRow.push(lastValue);
-            if (value) newRow.push(value);
+            if (lastValue) newRow.push(lastCell);
+            if (cell.value) newRow.push(cell);
         }
 
         return newRow
@@ -68,7 +78,7 @@ const resolveRow = (row, reverse) => {
 const getEmptyCells = () => {
     const emptyCells = []
     gameState.forEach((xList, y) => {
-        xList.forEach((value, x) => {
+        xList.forEach(({value}, x) => {
             if (value === 0) emptyCells.push({ x, y })
         })
     })
@@ -80,19 +90,30 @@ const createRandomBlock = () => {
     const emptyCells = getEmptyCells()
 
     if (!emptyCells.length) {
-        alert("Game over");
+        // alert("Game over");
     }
 
-    const { x, y } = emptyCells[Math.round(Math.random() * (emptyCells.length - 1))]
-    gameState[y][x] = Math.random() > 0.9 ? 4 : 2
+    // console.log(emptyCells)
+
+    const {x, y} = emptyCells[Math.round(Math.random() * (emptyCells.length - 1))]
+    gameState[y][x] = {
+        value: Math.random() > 0.9 ? 4 : 2,
+        id: generateRandomId()
+    }
+
+    createBlock(gameState[y][x].value, gameState[y][x].id, x, y)
+
 }
 
 
-const createBlock = (label) => {
-    const blockContainer = document.createElement("div")
-    blockContainer.classList.add("block-container")
+const createBlock = (label, id, x, y) => {
 
     if (label) {
+        const blockContainer = document.createElement("div")
+        blockContainer.classList.add("block-container")
+        blockContainer.style.transform = getCellTransition(x, y)
+        blockContainer.setAttribute("id", id)
+
         const block = document.createElement("div")
         block.classList.add("block", `block--${label}`)
 
@@ -103,19 +124,51 @@ const createBlock = (label) => {
 
         block.appendChild(blockLabel)
         blockContainer.appendChild(block)
+        gameContainer.appendChild(blockContainer)
     }
 
-    gameContainer.appendChild(blockContainer)
+}
+
+const cleanBoard = () => {
+    const nodes = gameContainer.childNodes
+
+    setTimeout(() => {
+        nodes.forEach((node) => {
+            if (!gameState.some((xList) => xList.some(({ id }) => node.id === id))) {
+                node.remove()
+            }
+        })
+    }, 100)
+
 }
 
 const paint = () => {
-    gameContainer.innerHTML = ""
+    // gameContainer.innerHTML = ""
 
-    gameState.forEach((xList) => {
-        xList.forEach((value) => {
-            createBlock(value)
+    gameState.forEach((xList, y) => {
+        xList.forEach(({ value, id }, x) => {
+            if (value) {
+                const element = document.getElementById(id)
+                element.style.transform = getCellTransition(x, y)
+                element.innerHTML = ""
+
+                const block = document.createElement("div")
+                block.classList.add("block", `block--${value}`)
+
+                const blockLabel = document.createElement("span")
+                block.classList.add("block__label")
+                blockLabel.innerText = value
+
+                block.appendChild(blockLabel)
+                element.appendChild(block)
+
+
+            }
+            // createBlock(value, id)
         })
     })
+
+    cleanBoard()
 }
 
 window.addEventListener("keydown", ({ code }) => {
@@ -123,9 +176,12 @@ window.addEventListener("keydown", ({ code }) => {
     if (!handler) return;
 
     handler()
+    console.log(gameState)
     createRandomBlock()
 
     paint()
 })
 
+createRandomBlock()
+createRandomBlock()
 paint()
